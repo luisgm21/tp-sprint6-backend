7a516f3b2dbec3ddc89f749345d35a1bf81208cf
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2026-05-12 14:19:06 -0300

    feat: implementar servidor con Express y conexión a MongoDB

--- server.mjs
+++ server.mjs
@@ -1,1 +1,18 @@
 import 'dotenv/config';
+import app from './src/app.mjs';
+import connectDB from './src/config/db.mjs';
+
+const startServer = async () => {
+  try {
+    const PORT = process.env.PORT || 3000;
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en el puerto ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error al iniciar el servidor:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
