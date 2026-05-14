8ef59eaf3fea5a63665fdda3e44656b7a02ebc14
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2026-05-14 05:54:25 -0300

    fix: corregir error en la llamada de las validaciones en las rutas

--- src/routes/userRoutes.mjs
+++ src/routes/userRoutes.mjs
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { userUpdateValidationRules } from "../validators/userValidators.mjs";
+import { userUpdateValidationRules, userValidationRules } from "../validators/userValidators.mjs";
 import { validate } from "../middleware/validatorMiddleware.mjs";
 import { getUsersController, createUserController, updateUserController, deleteUserController } from "../controllers/userController.mjs";
 
 const userRouter = Router();
 
 userRouter.get("/",getUsersController);
-userRouter.post("/create", createUserController);
-userRouter.put("/update/:id", userUpdateValidationRules, validate , updateUserController);
+userRouter.post("/create",userValidationRules(), validate, createUserController);
+userRouter.put("/update/:id", userUpdateValidationRules(), validate , updateUserController);
 userRouter.delete("/delete/:id", deleteUserController);
 
 export default userRouter;
