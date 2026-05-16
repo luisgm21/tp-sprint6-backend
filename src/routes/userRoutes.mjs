8cdab0b61a8798d50ae0501fc885b929d08309a2
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2026-05-16 18:38:49 -0300

    feat: agregar rutas para gestión de docentes y desactivación de usuarios

--- src/routes/userRoutes.mjs
+++ src/routes/userRoutes.mjs
@@ -1,15 +1,26 @@
 import { Router } from "express";
 import {changePasswordValidationRules ,userUpdateValidationRules, userValidationRules } from "../validators/userValidators.mjs";
 import { validate } from "../middleware/validatorMiddleware.mjs";
-import { changePasswordController , getUsersController , getUserController , createUserController, updateUserController, deleteUserController } from "../controllers/userController.mjs";
+import {
+	changePasswordController,
+	getUsersController,
+	getUserController,
+	createUserController,
+	updateUserController,
+	deleteUserController,
+	getTeachersBySchoolController,
+	deactivateUserController
+} from "../controllers/userController.mjs";
 
 const userRouter = Router();
 
 userRouter.get("/",getUsersController);
+userRouter.get("/school/:schoolId/teachers", getTeachersBySchoolController);
 userRouter.get("/:id", getUserController);
 userRouter.post("/create",userValidationRules(), validate, createUserController);
 userRouter.put("/update/:id", userUpdateValidationRules(), validate , updateUserController);
 userRouter.patch("/update/:id/password", changePasswordValidationRules(), validate, changePasswordController);
+userRouter.patch("/deactivate/:id", deactivateUserController);
 userRouter.delete("/delete/:id", deleteUserController);
 
 export default userRouter;
