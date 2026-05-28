49c41ff736ba78ca5a40d85ebef02a710b6fe9ba
Luis Gonzalo Morelli <gonzy.morelli@gmail.com>
2026-05-28 10:30:45 -0300

    feat: agregar redirección en la raíz a la ruta /api

--- src/app.mjs
+++ src/app.mjs
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (_req, res) => {
+	res.redirect('/api');
+});
+
 app.use("/api", router);
 
 export default app;
